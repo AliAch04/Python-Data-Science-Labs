88184203b17fcffcfd93acaef46cb8fb126ff748
Ali ACHENAN <ali.jallousy12@gmail.com>
2026-01-12 23:39:06 +0100

    Partie 7: Ddfinition d'Items et extraction complexe et Suivi des pages détails

--- TP6_Scrapy/mon_scraper/mon_scraper/spiders/books.py
+++ TP6_Scrapy/mon_scraper/mon_scraper/spiders/books.py
@@ -1,5 +1,6 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from mon_scraper.items import ProductItem 
 
 class BooksSpider(CrawlSpider):
     name = 'books'
@@ -7,19 +8,23 @@ class BooksSpider(CrawlSpider):
     start_urls = ['http://books.toscrape.com/']
     rules = (
         Rule(LinkExtractor(allow=r'catalogue/page-\d+\.html'), callback='parse_page', follow=True),
-        Rule(LinkExtractor(allow=r'catalogue/.+/index\.html'), callback='parse_book',follow=False),
+        Rule(LinkExtractor(allow=r'catalogue/.+/index\.html'), callback='parse_book', follow=False),
     )
 
     def parse_page(self, response):
         self.logger.info(f'Crawling page: {response.url}')
-        
 
     def parse_book(self, response):
-        yield {
-            'title': response.css('h1::text').get(),
-            'price': response.css('p.price_color::text').get(),
-            'availability': response.css('p.availability::text').re_first(r'\d+'),
-            'rating': response.css('p.star-rating::attr(class)').re_first(r'star-rating (\w+)'),
-            'description': response.css('article.product_page p::text').get(),
-            'url': response.url,
-        }
+        item = ProductItem()
+        
+        item['title'] = response.css('h1::text').get()
+        item['price'] = response.css('p.price_color::text').get()
+        item['availability'] = response.css('p.availability::text').re_first(r'\d+')
+        item['rating'] = response.css('p.star-rating::attr(class)').re_first(r'star-rating (\w+)')
+        item['description'] = response.css('article.product_page p::text').get()
+        item['url'] = response.url
+        
+        self.logger.info(f'Scraped book: {item["title"]} - {item["price"]}')
+        
+        # Yield l'item au lieu du dict
+        yield item
