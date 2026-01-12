6e143566a2769d1d7086abf9e3643b22e387519f
Ali ACHENAN <ali.jallousy12@gmail.com>
2026-01-12 22:48:04 +0100

    Partie 4: Implémentation du spider basique avec extraction et export de données

--- TP6_Scrapy/mon_scraper/mon_scraper/spiders/quotes.py
+++ TP6_Scrapy/mon_scraper/mon_scraper/spiders/quotes.py
@@ -1,10 +1,19 @@
 import scrapy
 
-
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     allowed_domains = ["quotes.toscrape.com"]
-    start_urls = ["https://quotes.toscrape.com"]
+    start_urls = ["http://quotes.toscrape.com/"]
 
     def parse(self, response):
-        pass
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
+        
+        # Suivre automatiquement le lien "Next"
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
